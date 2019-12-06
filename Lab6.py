8584f797a359121de8a27c9d63062768e685b6a5
YasashiisGod <54971293+YasashiisGod@users.noreply.github.com>
2019-12-06 11:36:36 -0700

    Add files via upload

--- Lab6.py
+++ Lab6.py
@@ -8,109 +8,90 @@ Assignment: Lab #6
 Instructor: Diego Aguirre 
 D.O.L.M.: 12/05/19 
 """
-
-class DisjointSetForest:
-    def __init__(self, n):
-        self.forest = [-1] * n
-
-    def is_index_valid(self, index):
-        return 0 <= index < len(self.forest)
-
-    # Problem 6
-    def find(self, a):
-        if not self.is_index_valid(a):
-            return -1
-
-        if self.forest[a] < 0:
-            return a
-
-        return self.find(self.forest[a])
-
-    # Problem 7
-    def find_contains_loop(self, a, s=None):
-        if not self.is_index_valid(a):
-            return -1
-
-        if s is None:
-            s = set()
-
-        if a in s:
-            print("Loop found")
-            return -1
-
-        s.add(a)
-
-        if self.forest[a] < 0:
-            return a
-
-        return self.find_contains_loop(self.forest[a], s)
-
-    def union(self, a, b):
-        ra = self.find(a)
-        rb = self.find(b)
-
-        if ra != rb:  # Problems 3 and 4
-            self.forest[rb] = ra
-
-    # Problem 2
-    def in_same_set(self, a, b):
-        if not self.is_index_valid(a) or not self.is_index_valid(b):
-            return False
-
-        return self.find(a) == self.find(b)
-
-    # Problem 5
-    def num_sets(self):
-        count = 0
-
-        for k in self.forest:
-            if k < 0:
-                count += 1
-
-        return count
-
-    # Problem 9
-    def is_equal(self, dsf):
-
-        if len(self.forest) != len(dsf.forest):
-            return False
-
-        # Running time O(n^2). Can you do it in O(n)? (:
-        for i in range(len(self.forest)):
-            for j in range(len(self.forest)):
-                if self.in_same_set(i, j) != dsf.in_same_set(i, j):
-                    return False
-
-        return True
-
-    def __str__(self):
-        return str(self.forest)
-
-
-# Problem 8
-def create_evens_odds_dsf(n):
-    dsf = DisjointSetForest(n)
-
-    for i in range(2, n):
-        dsf.union(i, i % 2)
-
-    return dsf
-
-
-def main():
-    dsf = DisjointSetForest(7)
-    print(dsf)
-    dsf.union(2, 3)
-    print(dsf)
-    dsf.union(0, 3)
-    print(dsf)
-    dsf.union(4, 5)
-    print(dsf)
-    dsf.union(3, 5)
-    print(dsf)
-    dsf.union(1, 4)
-    print(dsf)
-
-
-if __name__ == "__main__":
-    main()
+class Graph: 
+  
+    def __init__(self,vertices): 
+        self.V= vertices #No. of vertices 
+        self.graph = [] # default dictionary to store graph 
+   
+    # function to add an edge to graph 
+    def addEdge(self,u,v,w): 
+        self.graph.append([u,v,w]) 
+  
+    # A utility function to find set of an element i 
+    # (uses path compression technique) 
+    def find(self, parent, i): 
+        if parent[i] == i: 
+            return i 
+        return self.find(parent, parent[i]) 
+  
+    # A function that does union of two sets of x and y 
+    # (uses union by rank) 
+    def union(self, parent, rank, x, y): 
+        xroot = self.find(parent, x) 
+        yroot = self.find(parent, y) 
+  
+        # Attach smaller rank tree under root of  
+        # high rank tree (Union by Rank) 
+        if rank[xroot] < rank[yroot]: 
+            parent[xroot] = yroot 
+        elif rank[xroot] > rank[yroot]: 
+            parent[yroot] = xroot 
+  
+        # If ranks are same, then make one as root  
+        # and increment its rank by one 
+        else : 
+            parent[yroot] = xroot 
+            rank[xroot] += 1
+   # The main function to construct MST using Kruskal's  
+        # algorithm 
+    def KruskalMST(self): 
+  
+        result =[] #This will store the resultant MST 
+  
+        i = 0 # An index variable, used for sorted edges 
+        e = 0 # An index variable, used for result[] 
+  
+            # Step 1:  Sort all the edges in non-decreasing  
+                # order of their 
+                # weight.  If we are not allowed to change the  
+                # given graph, we can create a copy of graph 
+        self.graph =  sorted(self.graph,key=lambda item: item[2]) 
+  
+        parent = [] ; rank = [] 
+  
+        # Create V subsets with single elements 
+        for node in range(self.V): 
+            parent.append(node) 
+            rank.append(0) 
+      
+        # Number of edges to be taken is equal to V-1 
+        while e < self.V -1 : 
+  
+            # Step 2: Pick the smallest edge and increment  
+                    # the index for next iteration 
+            u,v,w =  self.graph[i] 
+            i = i + 1
+            x = self.find(parent, u) 
+            y = self.find(parent ,v) 
+  
+            # If including this edge does't cause cycle,  
+                        # include it in result and increment the index 
+                        # of result for next edge 
+            if x != y: 
+                e = e + 1     
+                result.append([u,v,w]) 
+                self.union(parent, rank, x, y)             
+            # Else discard the edge 
+  
+        # print the contents of result[] to display the built MST 
+        print ("Following are the edges in the constructed MST")  
+        for u,v,weight  in result: 
+            #print str(u) + " -- " + str(v) + " == " + str(weight) 
+            print ("%d -- %d == %d" % (u,v,weight))
+            
+def test():
+    
+    return 0
+    
+test()
